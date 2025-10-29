3524564ae7786a2556db9419ecebbe4164f6c541
miguelroman1 <23306080610314@cetis61.edu.mx>
2025-10-29 10:08:24 -0500

    practica

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("form.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
